af235202b0315ed19a123c7d6c4188336b56cc6d
gankhulugdev <ihcrew_hulguu@yahoo.com>
2022-10-19 21:34:27 -0500

    fixed 0. and 00 errors

--- Calculator/cal.js
+++ Calculator/cal.js
@@ -2,34 +2,33 @@ const numbers = document.getElementsByClassName("number");
 const operators = document.getElementsByClassName("operator");
 const display = document.getElementById("display");
 const equalSign = document.getElementById("equal");
-const dot = document.getElementById('dot');
+const dot = document.getElementById("dot");
 
 let num1 = null;
 let num2 = null;
+let result = null;
 let op = "";
 let isEqualPressed = false;
 let isZeroDot = false;
 
-
-
-const clearNumsOp = ()=>{
-    num1 = null;
-    num2 = null;
-    op = "";
-}
 const calculate = () => {
+  //when num1 has no value, assign display value to num1
+  if (num1 === null) num1 = parseFloat(display.innerHTML);
   //when num2 has no value, assign num1 value to num2
-  if (num2 === null) num2 = num1;
+  else if (num2 === null) num2 = num1;
+  //determine if equal sign is pressed
+  if (!isZeroDot && isEqualPressed) num1 = result;
+  
 
   switch (op) {
-    case "+": num1 += num2; break;
-    case "-": num1 -= num2; break;
-    case "/": num1 /= num2; break;
-    case "X": num1 *= num2; break;
+    case "+": result = num1 + num2; break;
+    case "-": result = num1 - num2; break;
+    case "/": result = num1 / num2; break;
+    case "X": result = num1 * num2; break;
     default: break;
   }
 
-  display.innerHTML = num1;
+  display.innerHTML = result;
 };
 
 display.innerHTML = 0;
@@ -39,33 +38,31 @@ for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", (event) => {
     if (!isEqualPressed) {
       if (!op) {
-        if (num1 || isZeroDot) {
+        if ((num1 !== 0 && num1 !== null) || isZeroDot) {
           display.innerHTML += event.target.innerHTML;
           num1 = parseFloat(display.innerHTML);
-          isZeroDot = (num1 === 0);
-
+          isZeroDot = num1 === 0;
         } else {
           display.innerHTML = event.target.innerHTML;
           num1 = parseFloat(display.innerHTML);
-          console.log(num1)
         }
       } else {
-        if (num2 || isZeroDot) {
+        if ((num2 !== 0 && num2 !== null) || isZeroDot) {
           display.innerHTML += event.target.innerHTML;
           num2 = parseFloat(display.innerHTML);
-          isZeroDot = (num2 === 0)
-
-        } else {
+          isZeroDot = num2 === 0;
+        } else if (num2 === null || num2 === 0) {
           display.innerHTML = event.target.innerHTML;
           num2 = parseFloat(display.innerHTML);
-
+        } else if ((num1 !== 0 && num1 !== null) || isZeroDot) {
+          display.innerHTML += event.target.innerHTML;
+          num1 = parseFloat(display.innerHTML);
+          isZeroDot = num1 === 0;
         }
       }
     } else {
-      clearNumsOp();
       display.innerHTML = event.target.innerHTML;
       num1 = parseFloat(display.innerHTML);
-
       isEqualPressed = false;
     }
   });
@@ -73,19 +70,28 @@ for (let i = 0; i < numbers.length; i++) {
 // listen operator signs
 for (let i = 0; i < operators.length; i++) {
   operators[i].addEventListener("click", (event) => {
-    if (!isEqualPressed && num2) {
-      calculate();
-      op = event.target.innerHTML;
-      num2 = null;
-    }else if(num1 === 0 && isZeroDot){
-        
+    if (!isEqualPressed) {
+      if (num2 !== null) {
+        calculate();
         op = event.target.innerHTML;
-        isZeroDot = false;
+        num1 = parseFloat(display.innerHTML);
+        num2 = null;
+      } else {
+        op = event.target.innerHTML;
+      }
     } else {
-      op = event.target.innerHTML;
-      num2 = null;
-    //   isZeroDot = true;
-      isEqualPressed = false;
+      if (num1 === 0) {
+        op = event.target.innerHTML;
+        num1 = parseFloat(display.innerHTML);
+        num2 = null;
+
+        isEqualPressed = false;
+      } else {
+        op = event.target.innerHTML;
+        num2 = null;
+
+        isEqualPressed = false;
+      }
     }
   });
 }
@@ -94,26 +100,28 @@ for (let i = 0; i < operators.length; i++) {
 equalSign.addEventListener("click", (event) => {
   calculate();
   isEqualPressed = true;
+  isZeroDot = false;
 });
 
 //listen dot
-dot.addEventListener('click', (event)=>{
-    if(!isEqualPressed){
-        if(num1 && op && !num2 && !isZeroDot){
-            display.innerHTML = '0.'
-            isZeroDot = true;
-            // num2 = parseFloat(display.innerHTML);
-            
-        }else if(!display.innerHTML.includes('.')){
-            display.innerHTML += event.target.innerHTML;
-            isZeroDot = (!num1 || !num2);
-        }
-    }else{
-        clearNumsOp();
-        display.innerHTML = '0.'
-        isZeroDot = true;
-        isEqualPressed = false;
+dot.addEventListener("click", (event) => {
+  if (!isEqualPressed) {
+    if (num1 !== null && op && num2 === null && !isZeroDot) {
+      display.innerHTML = "0.";
+      num2 = parseFloat(display.innerHTML);
+      isZeroDot = true;
+    } else if (num1 === null && !op & !isZeroDot) {
+      display.innerHTML = "0.";
+      num1 = parseFloat(display.innerHTML);
+      isZeroDot = true;
+    } else if (!display.innerHTML.includes(".")) {
+      isZeroDot = display.innerHTML === "0";
+      display.innerHTML += event.target.innerHTML;
     }
-    
-    
-})
+  } else {
+    display.innerHTML = "0.";
+    num1 = parseFloat(display.innerHTML);
+    isZeroDot = true;
+    isEqualPressed = false;
+  }
+});
